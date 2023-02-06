6a347523475ba148fe5a01c730147e8b9261f865
valere3162 <chayan@valere.io>
2023-02-06 19:25:53 +0530

    added login screen ui

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by MacMini03 on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
